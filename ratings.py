829d50a53825cc8e9ca22010f6390086eee7b88a
Mckena Ullman <ullman.kena@gmail.com>
2022-01-22 13:45:00 -0700

    User and input rest/score

--- ratings.py
+++ ratings.py
@@ -8,6 +8,12 @@ with open("scores.txt", 'r') as inf:
         dicts_from_file[key] = int(val)
 
 
+new_resturant = input("What resturant name would you like to add? ")
+new_resturant_score = input("What score would you give this resturant? ")
+
+dicts_from_file[new_resturant] = new_resturant_score
+
+
 sorted_keys = sorted(dicts_from_file.keys())
 for key in sorted_keys:
     print(key, dicts_from_file[key])
